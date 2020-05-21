1dd505edf55703d3cc0323970f3c2c87a095eaea
tringuyen1092000 <58884764+tringuyen1092000@users.noreply.github.com>
2020-05-21 18:41:24 +0700

    sau khi chia module

---
+++ Main.cpp
@@ -0,0 +1,11 @@
+#include "Lib.h"
+
+int main()
+{
+	string num = "1166B";
+	QInt* a = toQInt(num, 16);
+	cout << a->toBase2() << " " << a->toBase10() << " " << a->toBase16()<< endl;
+	//string test = "340282366920938463463374607431768211456";	
+	delete[] a;
+	return 0;
+}
