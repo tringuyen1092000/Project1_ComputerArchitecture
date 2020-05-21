f39c5dc6c43601171880041131b47bbc2402ef75
Nguyen Dinh Tri <tringuyen1092000@gmail.com>
2020-05-21 18:44:47 +0700

    update main

--- Main.cpp
+++ Main.cpp
@@ -5,7 +5,8 @@ int main()
 	string num = "1166B";
 	QInt* a = toQInt(num, 16);
 	cout << a->toBase2() << " " << a->toBase10() << " " << a->toBase16()<< endl;
-	//string test = "340282366920938463463374607431768211456";	
+	//string test = "340282366920938463463374607431768211456";
+	cout << "Test" << endl;
 	delete[] a;
 	return 0;
 }
