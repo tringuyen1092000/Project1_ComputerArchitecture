3ced182377f3433f8e817e7cdd94c60af2687387
hhthuonghcmus <65527547+hhthuonghcmus@users.noreply.github.com>
2020-05-23 16:53:46 +0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,12 @@
 #include "Lib.h"
 
-int main()
-{
-	string num = "1166B";
-	QInt* a = toQInt(num, 16);
-	cout << a->toBase2() << " " << a->toBase10() << " " << a->toBase16()<< endl;
-	//string test = "340282366920938463463374607431768211456";	
+int main() {
+	string num = "-170146037158805728";
+	QInt* a = toQInt(num, 10);
+	cout << "Number: " << num << endl << "Base 2: " << a->toBase2() << endl << "Base 10: " << a->toBase10() << endl << "Base 16: " << a->toBase16() << endl;
+	//string max = "170141183460469231731687303715884105727";
+	//string min = "-170141183460469231731687303715884105728";
 	delete[] a;
+	bitset<8> b(-5);
 	return 0;
 }
