8b6a604c810171f28b4f20dcf34c98d1696726bf
Nguyen Dinh Tri <tringuyen1092000@gmail.com>
2020-05-25 08:36:33 +0700

    Bo sung phan doc file

--- Main.cpp
+++ Main.cpp
@@ -8,5 +8,12 @@ int main()
 	//string test = "340282366920938463463374607431768211456";
 	cout << "Test" << endl;
 	delete[] a;
+	string inputStr = readFile("input.txt");
+	int p1 = 0, p2 = 0;
+	string str = readLine(27, inputStr);
+	string opr = operation(str, p1, p2);
+	cout << p1 << endl << p2 << endl;
+	cout << opr;
+	system("pause");
 	return 0;
 }
